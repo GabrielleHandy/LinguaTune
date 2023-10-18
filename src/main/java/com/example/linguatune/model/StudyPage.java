06c54b1870f6a8544a34908fe737ac26ae87c3da
GabrielleHandy <gehandy123@gmail.com>
2023-10-18 17:03:41 -0400

    Add getters, setters, and constructors to studyPage model

--- src/main/java/com/example/linguatune/model/StudyPage.java
+++ src/main/java/com/example/linguatune/model/StudyPage.java
@@ -37,5 +37,65 @@ public class StudyPage {
     private List<Song> recentSongs = new List<Song>();
 
 
+    public StudyPage() {
+    }
 
+    public StudyPage(Long id, User user, Language language, Set<FlashcardStack> flashcardStacks, List<Song> recentSongs) {
+        this.id = id;
+        this.user = user;
+        this.language = language;
+        this.flashcardStacks = flashcardStacks;
+        this.recentSongs = recentSongs;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    public Set<FlashcardStack> getFlashcardStacks() {
+        return flashcardStacks;
+    }
+
+    public void setFlashcardStacks(Set<FlashcardStack> flashcardStacks) {
+        this.flashcardStacks = flashcardStacks;
+    }
+
+    public List<Song> getRecentSongs() {
+        return recentSongs;
+    }
+
+    public void setRecentSongs(List<Song> recentSongs) {
+        this.recentSongs = recentSongs;
+    }
+
+    @Override
+    public String toString() {
+        return "StudyPage{" +
+                "id=" + id +
+                ", user=" + user.getUserName() +
+                ", language=" + language +
+                ", flashcardStacks=" + flashcardStacks +
+                ", recentSongs=" + recentSongs +
+                '}';
+    }
 }
