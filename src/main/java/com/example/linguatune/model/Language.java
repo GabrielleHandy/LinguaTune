ca36c35acb8e97164c3cfd20085dc682c808d622
GabrielleHandy <gehandy123@gmail.com>
2023-10-18 17:18:47 -0400

    Create language Model and added properties

--- src/main/java/com/example/linguatune/model/Language.java
+++ src/main/java/com/example/linguatune/model/Language.java
@@ -27,6 +27,57 @@ public class Language {
     @LazyCollection(LazyCollectionOption.FALSE)
     private List<StudyPage> studyPages;
 
+    public Language() {
+    }
+
+    public Language(Long id, String name, String languageCode, List<StudyPage> studyPages) {
+        this.id = id;
+        this.name = name;
+        this.languageCode = languageCode;
+        this.studyPages = studyPages;
+    }
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLanguageCode() {
+        return languageCode;
+    }
+
+    public void setLanguageCode(String languageCode) {
+        this.languageCode = languageCode;
+    }
+
+    public List<StudyPage> getStudyPages() {
+        return studyPages;
+    }
+
+    public void setStudyPages(List<StudyPage> studyPages) {
+        this.studyPages = studyPages;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Language{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", languageCode='" + languageCode + '\'' +
+                ", Number of StudyPages=" + studyPages.toArray().length +
+                '}';
+    }
 }
