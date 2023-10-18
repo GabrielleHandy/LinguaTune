76f8505359439567a8c811c5bcc7ba78298bc504
GabrielleHandy <gehandy123@gmail.com>
2023-10-18 16:39:38 -0400

    Add properties to user model

--- src/main/java/com/example/linguatune/model/UserModel.java
+++ src/main/java/com/example/linguatune/model/UserModel.java
@@ -1,11 +1,36 @@
 package com.example.linguatune.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class UserModel {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String userName;
+    @Column(unique = true)
+    private String emailAddress;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column
+    private String password;
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private studyPage studyPage;
+
+    public UserModel() {
+    }
+
+    public UserModel(Long id, String userName, String emailAddress, String password, studyPage studyPage) {
+        this.id = id;
+        this.userName = userName;
+        this.emailAddress = emailAddress;
+        this.password = password;
+        this.studyPage = studyPage;
+    }
+
 
 }
