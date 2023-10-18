13460b5b0427de2028608751652548cd3c8a2511
GabrielleHandy <gehandy123@gmail.com>
2023-10-18 16:36:26 -0400

    Create user Model

---
+++ src/main/java/com/example/linguatune/model/UserModel.java
@@ -0,0 +1,11 @@
+package com.example.linguatune.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserModel {
+
+}
