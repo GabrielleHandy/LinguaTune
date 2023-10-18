f8ae3cb0e842d4a954a3e09b15c95cb7e7ad154d
GabrielleHandy <gehandy123@gmail.com>
2023-10-18 16:45:05 -0400

    Create studyPage Model

---
+++ src/main/java/com/example/linguatune/model/StudyPageModel.java
@@ -0,0 +1,10 @@
+package com.example.linguatune.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "studypages")
+public class StudyPageModel {
+
+}
