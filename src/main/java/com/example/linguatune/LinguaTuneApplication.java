f5ee8da55107f9e4b78302dcc562cf96ea398663
GabrielleHandy <gehandy123@gmail.com>
2023-10-18 16:29:19 -0400

    Create springBoot project

---
+++ src/main/java/com/example/linguatune/LinguaTuneApplication.java
@@ -0,0 +1,13 @@
+package com.example.linguatune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinguaTuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinguaTuneApplication.class, args);
+	}
+
+}
