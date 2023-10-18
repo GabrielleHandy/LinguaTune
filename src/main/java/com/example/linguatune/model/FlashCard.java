309422b0a59e5d3f98b878e407ee2ffc88251589
GabrielleHandy <gehandy123@gmail.com>
2023-10-18 17:40:56 -0400

    Add getters, setters, and constructors to flashCard model

--- src/main/java/com/example/linguatune/model/FlashCard.java
+++ src/main/java/com/example/linguatune/model/FlashCard.java
@@ -20,6 +20,10 @@ public class FlashCard {
     @JoinColumn(name = "flashcardstack_id")
     private FlashCardStack cardStack;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JsonIgnore
+    @JoinColumn(name = "song_id")
+    private Song fromSong;
 
     @Column
     private String originalText;
@@ -28,7 +32,16 @@ public class FlashCard {
     private String translatedText;
 
 
+    public FlashCard() {
+    }
 
+    public FlashCard(Long id, FlashCardStack cardStack, Song fromSong, String originalText, String translatedText) {
+        this.id = id;
+        this.cardStack = cardStack;
+        this.fromSong = fromSong;
+        this.originalText = originalText;
+        this.translatedText = translatedText;
+    }
 
     public void setId(Long id) {
         this.id = id;
@@ -36,5 +49,48 @@ public class FlashCard {
 
     public Long getId() {
         return id;
+    }
+
+    public FlashCardStack getCardStack() {
+        return cardStack;
+    }
+
+    public void setCardStack(FlashCardStack cardStack) {
+        this.cardStack = cardStack;
+    }
+
+    public Song getFromSong() {
+        return fromSong;
+    }
+
+    public void setFromSong(Song fromSong) {
+        this.fromSong = fromSong;
+    }
+
+    public String getOriginalText() {
+        return originalText;
+    }
+
+    public void setOriginalText(String originalText) {
+        this.originalText = originalText;
+    }
+
+    public String getTranslatedText() {
+        return translatedText;
+    }
+
+    public void setTranslatedText(String translatedText) {
+        this.translatedText = translatedText;
+    }
+
+    @Override
+    public String toString() {
+        return "FlashCard{" +
+                "id=" + id +
+                ", cardStack=" + cardStack +
+                ", fromSong=" + fromSong.getTitle() + " by " + fromSong.getArtist() +
+                ", originalText='" + originalText + '\'' +
+                ", translatedText='" + translatedText + '\'' +
+                '}';
     }
 }
