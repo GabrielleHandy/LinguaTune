1cb16d76dbb27bf25ad7a30afbf88305d07fb632
GabrielleHandy <gehandy123@gmail.com>
2023-10-19 12:29:48 -0400

    Create seed file

---
+++ src/main/java/com/example/linguatune/seed/SeedData.java
@@ -0,0 +1,12 @@
+package com.example.linguatune.seed;
+import org.springframework.boot.CommandLineRunner;
+
+
+public class SeedData implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
