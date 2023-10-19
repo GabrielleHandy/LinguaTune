3325a195b6cafa463963fda1a43d5b7c4e6ec463
GabrielleHandy <gehandy123@gmail.com>
2023-10-19 18:17:30 -0400

    hid key

--- src/main/java/com/example/linguatune/seed/SeedData.java
+++ src/main/java/com/example/linguatune/seed/SeedData.java
@@ -1,10 +1,22 @@
 package com.example.linguatune.seed;
 import com.example.linguatune.model.Language;
+import com.example.linguatune.model.Song;
+import com.example.linguatune.model.StudyPage;
 import com.example.linguatune.model.User;
 import com.example.linguatune.repository.*;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
 
 @Component
 public class SeedData implements CommandLineRunner {
@@ -18,8 +30,13 @@ public class SeedData implements CommandLineRunner {
 
     private final StudyPageRepository studyPageRepository;
     private final SongRepository songRepository;
-
+    private ObjectMapper objectMapper = new ObjectMapper();
     private final TranslationRepository translationRepository;
+    private File updatedFrench = new File("C:\\Users\\gehan\\Desktop\\unit2\\Projects\\LinguaTune\\src\\main\\java\\com\\example\\linguatune\\seed\\frenchSongsUpdated.json");
+
+
+    @Value("rapidKey")
+    String RAPID_KEY;
 
     @Autowired
     public SeedData(UserRepository userRepository, FlashCardRepository flashRepository, FlashCardStackRepository flashCardStackRepository, LanguageRepository languageRepository, StudyPageRepository studyPageRepository, SongRepository songRepository, TranslationRepository translationRepository) {
@@ -31,25 +48,91 @@ public class SeedData implements CommandLineRunner {
         this.songRepository = songRepository;
         this.translationRepository = translationRepository;
     }
+    public void addLyrics(){
+        // Define the file path where the JSON file is located
+        try {
+            // Define the file path where the JSON file is located
+            String filePath = "C:\\Users\\gehan\\Desktop\\unit2\\Projects\\LinguaTune\\src\\main\\java\\com\\example\\linguatune\\seed\\frenchSongs.json";
+
+            // Create an ObjectMapper
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            // Read the JSON file and parse it into a List of TrackInfo objects
+            List<ImportedSong> trackInfoList = objectMapper.readValue(new File(filePath), new TypeReference<List<ImportedSong>>() {});
+
+            // Update the data within the list
+            for (ImportedSong track : trackInfoList) {
+                String uri = track.getTrack_uri().substring(track.getTrack_uri().indexOf("track:") + 6);
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create("https://spotify81.p.rapidapi.com/track_lyrics?id=" + uri))
+                        .header("X-RapidAPI-Key",RAPID_KEY)
+                        .header("X-RapidAPI-Host", "spotify81.p.rapidapi.com")
+                        .method("GET", HttpRequest.BodyPublishers.noBody())
+                        .build();
+                HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+                if(response.statusCode() == 200){
+                    track.setLyrics(objectMapper.writeValueAsString(response.body()));
+                }
+            }
+
+            // Convert the updated list to a JSON string
+            String updatedJsonString = objectMapper.writeValueAsString(trackInfoList);
+
+            // Write the updated JSON string back to the file
+            objectMapper.writeValue(new File("C:\\Users\\gehan\\Desktop\\unit2\\Projects\\LinguaTune\\src\\main\\java\\com\\example\\linguatune\\seed\\frenchSongsUpdated.json"), trackInfoList);
+
+            System.out.println("Data has been updated and written back to " + filePath);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 
     @Override
     public void run(String... args) throws Exception {
+        if(updatedFrench.length()==0){
+            addLyrics();
+        }
         Language English = new Language();
         English.setLanguageCode("en");
         English.setName("English");
-        languageRepository.saveAndFlush(English);
+        English = languageRepository.save(English);
 
         Language Spanish = new Language();
-        English.setLanguageCode("spa");
-        English.setName("Spanish");
-        languageRepository.saveAndFlush(Spanish);
+        Spanish.setLanguageCode("spa");
+        Spanish.setName("Spanish");
+        Spanish=languageRepository.save(Spanish);
 
         Language French = new Language();
-        English.setLanguageCode("fr");
-        English.setName("French");
-        languageRepository.saveAndFlush(French);
+        French.setLanguageCode("fr");
+        French.setName("French");
+        French = languageRepository.save(French);
 
 
+        User testUser = new User();
+        testUser.setNativeLanguage(English);
+        testUser.setPassword("111");
+        testUser.setUserName("LanguageLover");
+        testUser.setEmailAddress("test@test.com");
+        testUser = userRepository.save(testUser);
 
+        StudyPage testUserStudyPage = new StudyPage();
+        testUserStudyPage.setUser(testUser);
+        testUserStudyPage.setLanguage(French);
+        testUserStudyPage = studyPageRepository.save(testUserStudyPage);
+
+        List<ImportedSong> trackInfoList = objectMapper.readValue(updatedFrench, new TypeReference<List<ImportedSong>>() {});
+        for (ImportedSong track : trackInfoList){
+            if(track.getLyrics() != null) {
+                Song newSong = new Song();
+                newSong.setArtist(track.getArtists());
+                newSong.setLyrics("Index:"+ trackInfoList.indexOf(track
+                ));
+                newSong.setTitle(track.getTrack_name());
+                newSong.setPictureLink(track.getImage_uri());
+                newSong.setOriginal_lan(French.getLanguageCode());
+                songRepository.save(newSong);
+            }
+        }
     }
 }
