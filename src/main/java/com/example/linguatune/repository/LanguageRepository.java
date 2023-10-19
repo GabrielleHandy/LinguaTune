bb8943e8a8839093a99b8a846a6a4d0183945126
GabrielleHandy <gehandy123@gmail.com>
2023-10-19 12:44:25 -0400

    Create Language Repository

---
+++ src/main/java/com/example/linguatune/repository/LanguageRepository.java
@@ -0,0 +1,9 @@
+package com.example.linguatune.repository;
+
+import com.example.linguatune.model.Language;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LanguageRepository extends JpaRepository<Language, Long> {
+}
