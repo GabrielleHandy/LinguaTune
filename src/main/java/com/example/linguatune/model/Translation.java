874aceb6b5b3b95dcc070892b5c9132053c1539d
GabrielleHandy <gehandy123@gmail.com>
2023-10-19 12:23:44 -0400

    Add getters, setters, and constructors to Translation model

--- src/main/java/com/example/linguatune/model/Translation.java
+++ src/main/java/com/example/linguatune/model/Translation.java
@@ -15,7 +15,7 @@ public class Translation {
     @ManyToOne(fetch = FetchType.EAGER)
     @JsonIgnore
     @JoinColumn(name = "language_id")
-    private Song translation_lan;
+    private Language translation_lan;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JsonIgnore
@@ -25,4 +25,54 @@ public class Translation {
     @Column
     private Object lines;
 
+    public Translation() {
+    }
+
+    public Translation(Long id, Language translation_lan, Song translatedSong, Object lines) {
+        this.id = id;
+        this.translation_lan = translation_lan;
+        this.translatedSong = translatedSong;
+        this.lines = lines;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Language getTranslation_lan() {
+        return translation_lan;
+    }
+
+    public void setTranslation_lan(Language translation_lan) {
+        this.translation_lan = translation_lan;
+    }
+
+    public Song getTranslatedSong() {
+        return translatedSong;
+    }
+
+    public void setTranslatedSong(Song translatedSong) {
+        this.translatedSong = translatedSong;
+    }
+
+    public Object getLines() {
+        return lines;
+    }
+
+    public void setLines(Object lines) {
+        this.lines = lines;
+    }
+
+    @Override
+    public String toString() {
+        return "Translation{" +
+                "id=" + id +
+                ", translation_lan=" + translation_lan.getName() +
+                ", translatedSong=" + translatedSong.getTitle() +
+                '}';
+    }
 }
