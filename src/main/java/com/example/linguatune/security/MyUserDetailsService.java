49e03a18cdeea21fe4d2ae1016c84b99afe6a263
GabrielleHandy <gehandy123@gmail.com>
2023-10-20 17:28:44 -0400

    Made testGetUserById

--- src/main/java/com/example/linguatune/security/MyUserDetailsService.java
+++ src/main/java/com/example/linguatune/security/MyUserDetailsService.java
@@ -1,5 +1,8 @@
 package com.example.linguatune.security;
 
+import com.example.linguatune.model.User;
+import com.example.linguatune.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +10,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
+
+    UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
+        User user = userService.findByEmailAddress(username);
+        return new MyUserDetails(user);
     }
 }
--- src/test/java/com/example/linguatune/serviceTests/UserServiceTests.java
+++ src/test/java/com/example/linguatune/serviceTests/UserServiceTests.java
@@ -1,10 +1,19 @@
 package com.example.linguatune.serviceTests;
 
+import com.example.linguatune.model.Language;
+import com.example.linguatune.model.StudyPage;
+import com.example.linguatune.model.User;
 import com.example.linguatune.repository.UserRepository;
 import com.example.linguatune.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTests {
@@ -13,5 +22,23 @@ public class UserServiceTests {
     UserService userService;
     @Mock
     UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+
+        Language eng = new Language();
+        eng.setName("English");
+        eng.setLanguageCode("en");
+        eng.setId(1L);
+
+
+        User user = new User(1L, "LanguageLover", "test@test.com", eng, "111", new ArrayList<StudyPage>() );
+    }
+
+    @Test
+    public void testGetUserById(){
+
+    }
 
 }
