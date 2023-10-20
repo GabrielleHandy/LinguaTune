8264fe3cd2cbf988793885a8ef163149300b2037
GabrielleHandy <gehandy123@gmail.com>
2023-10-20 17:12:35 -0400

    Imported MockMVC and UserServiceTests created

---
+++ src/test/java/com/example/linguatune/serviceTests/UserServiceTests.java
@@ -0,0 +1,11 @@
+package com.example.linguatune.serviceTests;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTests {
+
+
+}
