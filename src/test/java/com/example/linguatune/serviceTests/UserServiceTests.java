89237377f1c5cbd796ecf837326d828e745600f8
GabrielleHandy <gehandy123@gmail.com>
2023-10-20 17:15:13 -0400

    Made User Service

--- src/test/java/com/example/linguatune/serviceTests/UserServiceTests.java
+++ src/test/java/com/example/linguatune/serviceTests/UserServiceTests.java
@@ -1,5 +1,7 @@
 package com.example.linguatune.serviceTests;
 
+import com.example.linguatune.repository.UserRepository;
+import com.example.linguatune.service.UserService;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -7,5 +9,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTests {
 
+    @Mock
+    UserService userService;
+    @Mock
+    UserRepository userRepository;
 
 }
