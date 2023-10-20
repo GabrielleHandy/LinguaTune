418277b4052a9b16ec219e625ee11be3473d0c6c
GabrielleHandy <gehandy123@gmail.com>
2023-10-20 17:53:56 -0400

    Made findById logic- test passed

--- src/main/java/com/example/linguatune/service/UserService.java
+++ src/main/java/com/example/linguatune/service/UserService.java
@@ -1,8 +1,12 @@
 package com.example.linguatune.service;
 
+import com.example.linguatune.exceptions.InformationNotFoundException;
+import com.example.linguatune.model.User;
 import com.example.linguatune.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -12,5 +16,17 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public User findById(Long id){
+        Optional<User> optionalUser = userRepository.findById(id);
+        if(optionalUser.isPresent()){
+            return optionalUser.get();
+        }
+        throw new InformationNotFoundException("User does not exist");
+    }
+
+    public User findByEmailAddress(String email){
+        return userRepository.findByEmailAddress(email);
     }
 }
