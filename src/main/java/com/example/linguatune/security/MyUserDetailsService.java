5caa384a207e48c778df4ef4298a74c656cb9117
GabrielleHandy <gehandy123@gmail.com>
2023-10-20 16:57:17 -0400

    Added findByEmailAddress in UserRepository

--- src/main/java/com/example/linguatune/security/MyUserDetailsService.java
+++ src/main/java/com/example/linguatune/security/MyUserDetailsService.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Service;
 public class MyUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+
     }
 }
