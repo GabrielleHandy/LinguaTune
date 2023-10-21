31c6b09f5bce1fb13105589180d8d091b39da56f
GabrielleHandy <91081738+GabrielleHandy@users.noreply.github.com>
2023-10-21 15:26:39 +0000

    Create deleteUserTest

--- src/test/java/com/example/linguatune/serviceTests/UserServiceTests.java
+++ src/test/java/com/example/linguatune/serviceTests/UserServiceTests.java
@@ -139,4 +139,14 @@ public class UserServiceTests {
         
 
     }
+    
+    @Test
+    public void testDeleteUser(){
+        when(userRepository.delete(any(User.class))).thenReturn(user);
+        User result = userServiceMock.deleteUser(1L);
+
+        assert(result.getUserName().equals(user.getUserName()));
+
+    }
+    
 }
