7d08e51be67b3b5b5855f6fb4a9c2e9ba01ef595
GabrielleHandy <91081738+GabrielleHandy@users.noreply.github.com>
2023-10-21 15:51:56 +0000

    Add basic properties in StudyPageService and StudyPageServiceTests

--- src/main/java/com/example/linguatune/service/StudyPageService.java
+++ src/main/java/com/example/linguatune/service/StudyPageService.java
@@ -1,10 +1,18 @@
 package com.example.linguatune.service;
 
+import java.util.ArrayList;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import com.example.linguatune.model.Language;
+import com.example.linguatune.model.StudyPage;
+import com.example.linguatune.model.User;
 import com.example.linguatune.repository.LanguageRepository;
 import com.example.linguatune.repository.StudyPageRepository;
+import com.example.linguatune.security.MyUserDetails;
 
 @Service
 public class StudyPageService {
@@ -15,9 +23,24 @@ public class StudyPageService {
 
     private final UserService userService;
 
+    private static User loggedinUser;
+
     @Autowired
     public StudyPageService(UserService userService, StudyPageRepository studyPageRepository){
         this.userService = userService;
         this.studyPageRepository = studyPageRepository;
     }
+
+    public void setTestLoggedInUser(User user){
+        loggedinUser = user;
+
+    }
+
+    public void setLoggedInUser() {
+        MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        loggedinUser = userDetails.getUser();
+        
+    }
+
+
 }
--- src/test/java/com/example/linguatune/serviceTests/StudyPageServiceTests.java
+++ src/test/java/com/example/linguatune/serviceTests/StudyPageServiceTests.java
@@ -1,10 +1,66 @@
 package com.example.linguatune.serviceTests;
 
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.linguatune.model.Language;
+import com.example.linguatune.model.StudyPage;
+import com.example.linguatune.model.User;
+import com.example.linguatune.repository.LanguageRepository;
+import com.example.linguatune.repository.StudyPageRepository;
+import com.example.linguatune.repository.UserRepository;
+import com.example.linguatune.security.MyUserDetails;
+import com.example.linguatune.service.StudyPageService;
+import com.example.linguatune.service.UserService;
 
 @ExtendWith(MockitoExtension.class)
 public class StudyPageServiceTests {
-    
-    
+
+     @Mock
+    UserService userService;
+
+
+
+    @InjectMocks
+    StudyPageService studyPageServiceMock;
+
+   
+   
+    @Mock
+    LanguageRepository languageRepository;
+
+    @Mock
+    StudyPageRepository studyPageRepository;
+
+    private static User user;
+    private Language eng;
+
+      @BeforeEach
+    public void setUp(){
+        MockitoAnnotations.openMocks(this);
+        studyPageServiceMock = new StudyPageService(userService, studyPageRepository);
+        
+
+
+
+        eng = new Language();
+        eng.setName("English");
+        eng.setLanguageCode("en");
+        eng.setId(1L);
+
+
+        user = new User(1L, "LanguageLover", "test@test.com", eng, "111", new ArrayList<StudyPage>() );
+        studyPageServiceMock.setTestLoggedInUser(user);
+    }
 }
+
+    
+
