62552a8e5500e3c72acc53c264f52e906fd355ea
GabrielleHandy <91081738+GabrielleHandy@users.noreply.github.com>
2023-10-21 15:35:50 +0000

    Create StudyPageSeviceTests

---
+++ src/test/java/com/example/linguatune/serviceTests/StudyPageServiceTests.java
@@ -0,0 +1,10 @@
+package com.example.linguatune.serviceTests;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class StudyPageServiceTests {
+    
+    
+}
