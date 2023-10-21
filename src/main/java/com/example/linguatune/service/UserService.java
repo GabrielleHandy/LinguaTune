246f9f812f9a0ab434b8951aef6a1c3afc5274dd
GabrielleHandy <gehandy123@gmail.com>
2023-10-20 21:11:51 -0400

    Create updateUser in user Service test passed

--- src/main/java/com/example/linguatune/service/UserService.java
+++ src/main/java/com/example/linguatune/service/UserService.java
@@ -1,14 +1,14 @@
 package com.example.linguatune.service;
 
 import com.example.linguatune.exceptions.InformationNotFoundException;
-import com.example.linguatune.model.Language;
 import com.example.linguatune.model.User;
 import com.example.linguatune.repository.LanguageRepository;
 import com.example.linguatune.repository.UserRepository;
+import com.example.linguatune.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -31,6 +31,12 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    public User setLoggedInUser() {
+        MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        loggedinUser = userDetails.getUser();
+        //solely for ease in testing
+        return userDetails.getUser();
+    }
 
     public User findById(Long id){
         Optional<User> optionalUser = userRepository.findById(id);
@@ -45,11 +51,31 @@ public class UserService {
     }
 
     public User createUser(User user) {
-
+        setLoggedInUser();
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setNativeLanguage(languageRepository.findById(1L).get());
 
         return userRepository.save(user);
+    }
+
+    public User updateUser(Long userId, User updatedUser) {
+        Optional<User> existingUserOptional = userRepository.findById(userId);
+        if (existingUserOptional.isPresent()) {
+            User existingUser = existingUserOptional.get();
+            if(updatedUser.getUserName() != null){
+                existingUser.setUserName(updatedUser.getUserName());
+
+            }
+            if(updatedUser.getPassword() != null){
+                existingUser.setPassword(passwordEncoder.encode(updatedUser.getPassword()));
+
+            }
+
+            return userRepository.save(existingUser);
+        } else {
+
+            throw new IllegalArgumentException();
+        }
     }
 
 
--- src/test/java/com/example/linguatune/serviceTests/UserServiceTests.java
+++ src/test/java/com/example/linguatune/serviceTests/UserServiceTests.java
@@ -6,16 +6,27 @@ import com.example.linguatune.model.StudyPage;
 import com.example.linguatune.model.User;
 import com.example.linguatune.repository.LanguageRepository;
 import com.example.linguatune.repository.UserRepository;
+import com.example.linguatune.security.MyUserDetails;
 import com.example.linguatune.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.naming.Context;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -24,28 +35,46 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
+
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTests {
 
 
     @Mock
     UserRepository userRepository;
+
+
+
     @InjectMocks
     UserService userServiceMock;
 
+    @Mock
+    MyUserDetails myUserDetails;
     @Mock
     PasswordEncoder passwordEncoder;
     @Mock
     LanguageRepository languageRepository;
 
+    @Mock
+    SecurityContextHolder securityContextHolder;
 
-    private User user;
+
+
+
+
+
+    public  UsernamePasswordAuthenticationToken authentication;
+    private static User user;
     private Language eng;
+
 
     @BeforeEach
     public void setUp(){
-        userServiceMock.setUserRepository(userRepository);
         MockitoAnnotations.initMocks(this);
+        userServiceMock.setUserRepository(userRepository);
+
+
+
 
         eng = new Language();
         eng.setName("English");
@@ -89,11 +118,14 @@ public class UserServiceTests {
     }
     @Test
     public void testUpdateUser(){
-        User updated = user;
-        updated.setUserName("Updated");
+        User updated = new User();
+        updated.setUserName("Bloop");
+
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
         when(userRepository.save(any(User.class))).thenReturn(updated);
-        User result = userServiceMock.updateUser(user);
-        assertNotEquals(result.getUserName(), user.getUserName());
+        User result = userServiceMock.updateUser(1L, updated);
+
+        assert(result.getUserName().equals(user.getUserName()));
 
     }
 }
